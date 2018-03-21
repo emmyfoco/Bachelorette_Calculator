61150c185ea6fbd92df2dd5265e4b66bbb2ceb3e
Emily Coleman <emily.foerster@gmail.com>
2018-03-21 06:53:21 -0400

    Added jQuery file and js notes

---
+++ app.js
@@ -0,0 +1,23 @@
+//jQuery('.box').hide();
+//$('.box').hide();
+//the two lines above are identical
+
+//box.addEventListener('click', function(){
+//    alert('You clicked me!'); 
+// });
+
+//above works same as below
+
+// $('.box').click(function(){
+//     alert('You clicked me!');
+// };)
+
+// $('#flashMessage').hide();
+// $('#flashMessage').slideDown(1000);
+// $('#flashMessage').delay(3000);
+// $('#flashMessage').slideUp();
+
+//above can chain all of the above together like this:
+
+//$('#flashMessage').hide().slideDown(1000).delay(3000).slideUp();
+
